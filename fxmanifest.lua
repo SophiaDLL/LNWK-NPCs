4f696aa0432475b6b506972d03c367c25280fe62
Sophia <sophia@zwrks.com>
2025-01-05 10:39:39 +0000

    Added the NUI Requirements

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,26 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'laurelnwk.com'
-description 'A simple script that allows you to spawn peds Via Modles/MP Clothing with animations'
+author 'laurelnwk.com/team'
+description 'A Simple FiveM Script that allows you spawn Both Multi-Player & Single-Player Peds'
 lua54 'yes'
-version '1.0.0'
+version '1.0.1'
+
+
 
 client_script 'client.lua'
 shared_script 'config.lua'
+ui_page 'html/index.html'
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+--dependencies {
+--    
+--}
+
+
+-- FUTURE DEPENDANCYS
+    --'ox_target' -- DO NOT UN COMMENT THIS, THIS IS FOR FUTURE UPDATES THAT ARE NOT CURRENTLY BEING USED.
