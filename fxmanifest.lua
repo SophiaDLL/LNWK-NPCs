ffe86ec04ffbcb04018908c528ceb4e63d7b9fc3
Kasey Fitton <kaseyfitton@hotmail.co.uk>
2025-01-05 07:52:50 +0000

    enable lua5.4

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,7 @@ game 'gta5'
 
 author 'laurelnwk.com'
 description 'A simple script that allows you to spawn peds Via Modles/MP Clothing with animations'
+lua54 'yes'
 version '1.0.0'
 
 client_script 'client.lua'
